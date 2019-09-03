63b8eee2615d9be7f6675757571546862ed5090d
Nick Burkhalter <nickburkhalter@Nicks-MacBook-Pro.local>
2019-09-03 11:13:26 -0700

    edited eda function

--- lambdata_nburkhal/eda.py
+++ lambdata_nburkhal/eda.py
@@ -1,8 +1,8 @@
 import numpy as np
-import pandas as import pd
+import pandas as pd
 
 # Get descriptive statistics for numeric and non-numeric columns of a dataframe
 
 def descriptive(df):
 
-    return df.describe, df.describe(exclude='number')
+    return df.describe(), df.describe(exclude='number')
